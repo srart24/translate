7c4c739fb4521bfb1100fe7d25eadc1bfbed2e25
srart24࿐ <mysrart24@gmail.com>
2023-12-23 13:36:40 +0700

    @srart24

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const translate = require('@vitalets/google-translate-api');
+const app = express();
+const port = 3000;
+
+app.set('json spaces', 2);
+app.use(express.json());
+
+const defaultLang = 'id';
+
+app.get('/api', async (req, res) => {
+  let { lang, text } = req.query;
+
+  if ((lang || '').length !== 2) {
+    lang = defaultLang;
+    text = req.query.text;
+  }
+
+  if (!text) {
+    return res.status(400).json({ code: 400, error: 'Parameter text diperlukan' });
+  }
+
+  try {
+    const result = await translate(text, { to: lang, autoCorrect: true });
+    res.json({ code: 200, result: result.text });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ code: 500, error: 'Terjadi kesalahan dalam proses terjemahan' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
