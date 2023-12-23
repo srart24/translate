1212b6a44bbe43e89db036513bb0550abe101d15
srart24࿐ <mysrart24@gmail.com>
2023-12-23 13:53:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use(express.json());
 
 const defaultLang = 'id';
 
+app.get('/', (req, res) => {
+  res.status(200).json({ 
+			status: true,
+      creator: `@srart24`,
+      code: 200,
+      message: "Follow Instagram @srart24"
+		});
+}); 
+
 app.get('/api', async (req, res) => {
   let { lang, text } = req.query;
 
@@ -17,12 +26,12 @@ app.get('/api', async (req, res) => {
   }
 
   if (!text) {
-    return res.status(400).json({ code: 400, error: 'Parameter text diperlukan' });
+    return res.status(400).json({ creator: "@srart24", code: 400, error: 'Parameter text diperlukan' });
   }
 
   try {
     const result = await translate(text, { to: lang, autoCorrect: true });
-    res.json({ code: 200, result: result.text });
+    res.json({ creator: "@srart24", code: 200, result: result.text });
   } catch (error) {
     console.error(error);
     res.status(500).json({ code: 500, error: 'Terjadi kesalahan dalam proses terjemahan' });
